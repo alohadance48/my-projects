01b44f2d47fefacce695e2f87c1b80b9d95de4b5
Vlad Afonskiy <146872011+alohadance48@users.noreply.github.com>
2024-11-18 02:38:53 +0300

    Add files via upload

--- deastole.py
+++ deastole.py
@@ -1,5 +1,5 @@
 import time
-from info import test
+
 
 
 
--- info.py
+++ info.py
@@ -1,25 +1,20 @@
-class info:
-    def deastole(self,status:bool)->bool:
-        '''Здесь будет передача в info_main'''
+# info.py
+class Info:
+    def deastole(self, status: bool) -> bool:
+        print(f"Info: Blood status is now {'Deastole' if status else 'Sistole'}")
+        return status
+
+    def user_info(self, is_old: int):
         pass
 
-    def user_info(self,is_old:int):
-        '''Здесь будет передача в info_main'''
+    def defects(self, not_full_heart: bool, deabet: bool, olf: bool):
         pass
 
-    def defects(self,not_full_heart:bool,deabet:bool,olf:bool):
-        '''Здесь будет передача в info_main'''
+    def dead(self, start: bool):
         pass
 
-    def dead(self,start:bool):
-        '''Здесь будет передача в info_main'''
+    def pulse(self, status: int):
         pass
 
-    def pulse(self,status:int):
-        '''Здесь будет передача в info_main'''
+    def end(self, end: bool):
         pass
-
-    def end(self,end:bool):
-        '''Здесь будет передача в info_main'''
-        pass
-    
