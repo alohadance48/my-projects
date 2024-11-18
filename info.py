d5f08c60bb0b1d8c22c86536a2faa3d0a3db3605
Vlad Afonskiy <146872011+alohadance48@users.noreply.github.com>
2024-11-18 04:07:35 +0300

    Add files via upload

--- info.py
+++ info.py
@@ -5,8 +5,8 @@ class Info:
         print(f"Info: Blood status is now {'Deastole' if status else 'Sistole'}")
         return status
 
-    def user_info(self, is_old: int):
-        print(f'Info:you old:{is_old}')
+    def user_info(self, is_old: int,fatigue:int):
+        print(f'Info:you old:{is_old}твоя нагрузка:{fatigue}')
         return is_old
 
     def defects(self, not_full_heart: bool, deabet: bool, olf: bool):
