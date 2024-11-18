525411a756f6ccffa4cf902772be93891cc5473d
Vlad Afonskiy <146872011+alohadance48@users.noreply.github.com>
2024-11-18 03:52:52 +0300

    Add files via upload

--- dead.py
+++ dead.py
@@ -1,21 +1,30 @@
 import time
 import random
+import threading
 
+def timer_callback():
+    print('Таймер завершен! Вы не успели сбросить нагрузку.')
 
-def heart_failure(fatigue:int,old:int,start:bool):
-    if fatigue == 100 and old >= 55 and start == True :
-        print('У вас огромная нагрузка на сердце , если в течение 10 секунд не сбросить нагрузку вы умрете с шансом 70 %')
-        time.sleep(10)
-        chance = random.randint(1,10)
-        if chance <= 7 :
-            print('Вы погибли')
+def heart_failure(fatigue: int, old: int, start: bool):
+    if fatigue >= 70 and old >= 55 and start:
+        print('У вас огромная нагрузка на сердце. Если в течение 10 секунд не сбросить нагрузку, вы умрете с шансом 70 %.')
+
+        timer = threading.Timer(10.0, timer_callback)
+        timer.start()
+
+        for i in range(10):
+            time.sleep(1)
+            test = input('Для сброса нагрузки напиши stop: ')
+            if test.lower() == 'stop':
+                timer.cancel()
+                print('Вы сбросили нагрузку!.')
+                print('Вы выжили ')
+                return
+
+        chance = random.randint(1, 10)
+        if chance <= 7:
+            print('Вы погибли.')
             dead = True
-            pass
-        else :
-            print('Вы выжили !')
+        else:
+            print('Вы выжили!')
             dead = False
-            pass
-
-
-
-heart_failure(100,55,True)
--- user.py
+++ user.py
@@ -1,18 +1,22 @@
 from old import User
 from info import Info
+from dead import heart_failure
 
 def main():
     class DescriptionOfTheHeart:
         def __init__(self):
             self.user_old = int(input('Сколько лет организму: '))
+            self.fatigue = int(input('Твоя нагрузка (от 0 до 100): '))
 
         def paraments(self):
             user_age = self.user_old
             start = User(user_age)
-            chance = start.chance_main()  # Получаем шанс недуга
+            chance = start.chance_main()
             main_info = Info()
-            main_info.user_info(user_age)
+            main_info.user_info(user_age, self.fatigue)
             print(f'Симуляция началась, ваши параметры: года {user_age}, шанс недуга: {chance}')
+            if self.user_old >= 55 and self.fatigue >= 70 :
+                heart_failure(self.fatigue, user_age, True)
 
     test = DescriptionOfTheHeart()
     test.paraments()
