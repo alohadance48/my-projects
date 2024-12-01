0398c58a7a04f751bf360cce6fdf38f442d33f9f
alohadance48 <afonskiy.vlad123@gmail.com>
2024-12-01 20:44:05 +0300

    error' eror

--- lessons/lesson_all.py
+++
@@ -1,4 +0,0 @@
-def is_leap_year(year:int)->bool:
-    return year % 400 == 0 or year % 4 == 0 and year % 100 != 0
-
-print(is_leap_year(2015))
--- python/people/test.py
+++
@@ -1,7 +0,0 @@
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
-#testq  23q2e q2 eq2w eqweq we qwa пиздец lol
