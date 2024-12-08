46b0b3aed6ed93bd148502db9b760115e172a23e
RUKE101 <168942545+RUKE101@users.noreply.github.com>
2024-12-08 21:08:32 +0300

    functional increase

--- lesson_y/tg_bot.py
+++ lesson_y/tg_bot.py
@@ -1,8 +1,9 @@
 import telebot
 from telebot import types
 import time
+import math
 
-TOKEN = '7576640329:AAE5aLRnF_nPcT-qzsGpZ9b2aE7OIAdEO8Y'  # Замените на ваш токен
+TOKEN = '7414301624:AAEXVOmLWkZxskSA4EeFxyY-rUupJr5flSI'  # Замените на ваш токен
 bot = telebot.TeleBot(TOKEN)
 
 # Создание объекта для хранения данных (для обработки поэтапных запросов)
@@ -90,6 +91,96 @@ def basic_task(message):
                                            "2. Скорость (в м/с)\n"
                                            "Введите массу (в кг):")
         user_data[message.chat.id] = {"task": "импульс"}
+
+@bot.message_handler(func=lambda message: user_data.get(message.chat.id, {}).get("category") == "МКТ")
+def mkt(message):
+    if message.text.lower() == "давление газа":
+        bot.send_message(message.chat.id, "Для расчета давления газа нужны следующие данные:\n"
+                                           "1. Количество вещества газа (моль, n)\n"
+                                           "2. Температура газа (в Kельвинах, T)\n"
+                                           "3. Объем газа (м³, V)"
+                                           "Введите все переменные подряд в формате без запятых. Пример: 2 4 6")
+        user_data[message.chat.id] = {"task": "давление газа"}
+    elif message.text.lower() == "средняя скорость молекул":
+        bot.send_message(message.chat.id, "Для расчета средней скорости молекул нужны следующие данные:\n"
+                                           "1. Температура газа (в Kельвинах, T)\n"
+                                           "2. Масса одной молекулы газа (кг, m)"
+                                           "Введите все переменные подряд в формате без запятых. Пример: 2 4")
+        user_data[message.chat.id] = {"task": "средняя скорость молекул"}
+
+@bot.message_handler(func=lambda message: user_data.get(message.chat.id, {}).get("category") == "Термодинамика")
+def thermodynamics(message):
+    if message.text.lower() == "работа газа":
+        bot.send_message(message.chat.id, "Для расчета работы газа нужны следующие данные:\n"
+                                          "1. Давление газа (в Дж, P)\n"
+                                          "2. Изменение объема газа (м³, ΔV)"
+                                          "Введите все переменные подряд в формате без запятых. Пример: 2 4")
+        user_data[message.chat.id] = {"task": "работа газа"}
+
+@bot.message_handler(func=lambda message: user_data.get(message.chat.id, {}).get("category") == "Цепи")
+def thermodynamics(message):
+    if message.text.lower() == "сила тока в цепи":
+        bot.send_message(message.chat.id, "Для расчета силы тока в цепи нужны следующие данные:\n"
+                                          "1. Напряжение (в Вольтах, В)\n"
+                                          "2. Сопротивление в Омах, Ω)"
+                                          "Введите все переменные подряд в формате без запятых. Пример: 2 4")
+        user_data[message.chat.id] = {"task": "сила тока в цепи"}
+
+
+@bot.message_handler(func=lambda message: user_data.get(message.chat.id, {}).get("task") == "давление газа")
+def pressure(message):
+    message_str = str(message.text)
+    message_list = message_str.split()
+    try:
+        n = float(message_list[0])
+        t = float(message_list[1])
+        v = float(message_list[2])
+        r = 8.314
+        answer = (n * r * t) / v
+        bot.send_message(message.chat.id, f'Ответом будет {answer}')
+
+    except ValueError:
+        bot.send_message(message.chat.id, "Неверный формат данных, попробуйте снова.")
+
+@bot.message_handler(func=lambda message: user_data.get(message.chat.id, {}).get("task") == "средняя скорость молекул")
+def molecule_speed(message):
+    message_str = str(message.text)
+    message_list = message_str.split()
+    try:
+        t = float(message_list[0])
+        m = float(message_list[1])
+        k = 1.38e-23
+        answer = math.sqrt((3 * k * t) / m)
+        bot.send_message(message.chat.id, f'Ответом будет {answer}')
+    except ValueError:
+        bot.send_message(message.chat.id, "Неверный формат данных, попробуйте снова.")
+
+@bot.message_handler(func=lambda message: user_data.get(message.chat.id, {}).get("task") == "работа газа")
+def gas_operation(message):
+    message_str = str(message.text)
+    message_list = message_str.split()
+    try:
+        p = float(message_list[0])
+        delta_v = float(message_list[1])
+        answer = p * delta_v
+        bot.send_message(message.chat.id, f'Ответом будет {answer}')
+    except ValueError:
+        bot.send_message(message.chat.id, "Неверный формат данных, попробуйте снова.")
+
+@bot.message_handler(func=lambda message: user_data.get(message.chat.id, {}).get("task") == "сила тока в цепи")
+def current_in_the_circuit(message):
+    message_str = str(message.text)
+    message_list = message_str.split()
+    try:
+        u = float(message_list[0])
+        r = float(message_list[1])
+        answer = u / r
+        bot.send_message(message.chat.id, f'Ответом будет {answer}')
+    except ValueError:
+        bot.send_message(message.chat.id, "Неверный формат данных, попробуйте снова.")
+
+
+
 
 # Запрос данных для расчета скорости
 @bot.message_handler(func=lambda message: user_data.get(message.chat.id, {}).get("task") == "скорость")
