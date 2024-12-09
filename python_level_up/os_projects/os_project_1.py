8c6a89e67bb0886d424cb8f76f17bb223100fdf9
alohadance48 <afonskiy.vlad123@gmail.com>
2024-12-09 22:38:46 +0300

    Новыый проект

--- python_level_up/os_projects/os_project_1.py
+++ python_level_up/os_projects/os_project_1.py
@@ -1,6 +1,25 @@
-def main():
-    pass
+import os
 
+class Sort_for_file:
+    def __init__(self):
+        self.direct = '/home/vlados/sort'
 
-if __name__ == '__main__':
-    main()
+class Sort_for_file_main(Sort_for_file):
+    def sort_file(self):
+        py_files = [f for f in os.listdir(self.direct)
+                    if os.path.isfile(os.path.join(self.direct, f)) and f.lower().endswith('.py')]
+        if py_files:
+            print('Найдены файлы!')
+            for file in py_files:
+                souce = os.path.join(self.direct,file)
+                desrination_path = os.path.join(os.path.expanduser('~/python_os'),file)
+                os.rename(souce,desrination_path)
+                print(f'Файлы были перемецины в {desrination_path}')
+                print('Файлы которф там находятся :')
+                os.chdir('/home/vlados/python_os')
+                print(os.popen('ls').read())
+        else :
+            print('Таких файлов нет')
+
+start = Sort_for_file_main()
+start.sort_file()
