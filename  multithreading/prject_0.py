ad50fb46189bb0929ff50f1806fd53121fb5b71c
alohadance48 <afonskiy.vlad123@gmail.com>
2024-12-14 02:20:06 +0300

    Многопоточность

---
+++  multithreading/prject_0.py
@@ -0,0 +1,33 @@
+import threading
+import requests
+
+def main():
+    class Get:
+        def __init__(self):
+            self.link = 'https://www.google.com/'
+            self.threads = []
+
+
+        def get_main(self):
+
+            get = requests.get(self.link)
+            print(get)
+
+        def thread(self):
+            for _ in range(4):
+                self.thread = threading.Thread(target=self.get_main())
+                self.threads.append(self.thread)
+                self.thread.start()
+
+        def end(self):
+            for self.thread in self.threads:
+                self.thread.join()
+                print('Все потоки завершины.')
+
+    get_m = Get()
+    get_m.thread()
+    get_m.end()
+    pass
+
+if __name__ == '__main__':
+    main()
